6b5d852e05828a26e8bfc2c8776f0cd0d68179e4
IvarK <ivarkera@gmail.com>
2018-11-20 14:18:25 +0200

    fixing stuff

--- format.js
+++ format.js
@@ -26,6 +26,10 @@ const FORMATLIST = ['', 'K', 'M', 'B', 'T', 'Qa', 'Qt', 'Sx', 'Sp', 'Oc', 'No', 
 
 const commaRegexp = /\B(?=(\d{3})+(?!\d))/g;
 
+const HAS_DECIMAL_LIBRARY = typeof Decimal === "function" // This checks whether Decimal has been imported or not, so we know to use Decimal or Math libraries
+
+const m = HAS_DECIMAL_LIBRARY ? "Decimal" : "Math"
+
 function letter(power,str) {
   const len = str.length;
   function lN(n) {
@@ -76,52 +80,42 @@ const Format = {
     return power.toString().replace(commaRegexp, ",")
   },
   scientific: function(power, mantissa) {
-    if (power > 100000) return (mant + "e" + this.addCommas(power))    
-    else return (mant + "e" + power);
+    if (power > 100000) return (mantissa + "e" + this.addCommas(power))    
+    else return (mantissa + "e" + power);
   },
   engineering: function(power, mantissa) {
     let pow = power - (power % 3)
-    let mant = (mantissa * Decimal.pow(10, pow % 3)).toFixed(PLACES)
-    if (mant >= 1000) {if (power > 100000  && !COMMAS) return "ee"+Math.log10(Decimal.log10(value)).toFixed(3)
-        if (power > 100000  && COMMAS) return "e" + formatWithCommas(Decimal.log10(value).toFixed(PLACES));
-        else return "e"+Decimal.log10(value).toFixed(Math.max(PLACES, 1))
-      mant /= 1000;
-      pow++;
-    }
-    
     if (pow > 100000) pow = addCommas(pow)
     return (mantissa + "e" + pow);
   },
   standard: function(power, mantissa) {
     let pow = power - (power % 3)
-    let mant = (mantissa).toFixed(PLACES)
-    
-    if (mant >= 1000) {
-      mant /= 1000;
-      pow++;
-    }
-    
-    if (pow <= 303) return mant + " " + FORMATLIST[pow / 3];
-    else return mant + " " + getAbbreviation(pow);
+    if (pow <= 303) return mantissa + " " + FORMATLIST[pow / 3];
+    else return mantissa + " " + getAbbreviation(pow);
   },
-  logarithm: function(power, mantissa) {
-    if (power > 100000) return "e" + addCommas(Decimal.log10(value).toFixed(PLACES))
-    else return "e"+Decimal.log10(value).toFixed(Math.max(PLACES, 1))
+  logarithm: function(value, power) {
+    if (power > 100000) return "e" + addCommas(window[m].log10(value).toFixed(PLACES))
+    else return "e"+window[m].log10(value).toFixed(Math.max(PLACES, 1))
+  },
+  letters: function(power, mantissa) {
+    return mantissa + letter(power,'abcdefghijklmnopqrstuvwxyz');
   }
 }
 
 
-function format(value) {
-  var notation = not
+function format(value, notation) {
+  if (notation === undefined) notation = not
   if ( value >= 1000 ) {
+    
     let power, mantissa;
-    if (value instanceof Decimal) {
+    if (HAS_DECIMAL_LIBRARY && value instanceof Decimal) {
       power = value.e
       mantissa = value.mantissa
     } else {
       mantissa = value / Math.pow(10, Math.floor(Math.log10(value)));
       power = Math.floor(Math.log10(value));
     }
+    
     if ((notation === "Mixed scientific" && power >= MIXED_POWER_THRESHOLD) || notation === "Scientific") {
       mantissa = mantissa.toFixed(PLACES)
       if (mantissa >= 10) {
@@ -131,36 +125,23 @@ function format(value) {
       return Format.scientific(power, mantissa)
     }
 
-    if (notation === "Logarithm") return Format.logarithm()
+    if (notation === "Logarithm") return Format.logarithm(value, power)
 
-    mantissa = (mantissa * Decimal.pow(10, power % 3)).toFixed(PLACES)
+    mantissa = (mantissa * window[m].pow(10, power % 3)).toFixed(PLACES)
     if (mantissa >= 1000) {
         mantissa /= 1000;
         power++;
     }
 
-    if (notation === "Standard" || notation === "Mixed scientific") {
-        if (power <= 303) return mantissa + " " + FormatList[(power - (power % 3)) / 3];
-        else return mantissa + " " + getAbbreviation(power);
-    } else if (notation === "Mixed engineering") {
-        if (power <= 33) return mantissa + " " + FormatList[(power - (power % 3)) / 3];
-        else return (mantissa + "e" + pow);
-    } else if (notation === "Engineering") {
-        return (mantissa + "e" + pow);
-    } else if (notation === "Letters") {
-        return mantissa + letter(power,'abcdefghijklmnopqrstuvwxyz');
-    } else if (notation === "Cancer") {
-        return mantissa + letter(power,['😠', '🎂', '🎄', '💀', '🍆', '👪', '🌈', '💯', '🍦', '🎃', '💋', '😂', '🌙', '⛔', '🐙', '💩', '❓', '☢', '🙈', '👍', '☂', '✌', '⚠', '❌', '😋', '⚡'])
-    }
-
-    else {
-        if (power > 100000  && COMMAS) power = formatWithCommas(power);
-        return "1337 H4CK3R"
-    }
-  } else if (value < 1000) {
-      return (value).toFixed(PLACES_UNDER_1000);
+    if (notation === "Standard" || notation === "Mixed scientific" || 
+       (notation === "Mixed Engineering" && power <= MIXED_POWER_THRESHOLD)) return Format.standard(power, mantissa)
+    else if (notation === "Engineering" || notation === "Mixed Engineering") return Format.engineering(power, mantissa);
+    else if (notation === "Letters") return Format.letters(power, mantissa)
   }
+  
+  return (value).toFixed(PLACES_UNDER_1000);
 }
 
-Object.defineProperty(Number.prototype, "format", { value: function() { return format(this) }})
+Object.defineProperty(Number.prototype, "format", { value: function(notation) { return format(this, notation) }})
+if (HAS_DECIMAL_LIBRARY) Object.defineProperty(Decimal.prototype, "format", { value: function(notation) { return format(this, notation) }})
 
